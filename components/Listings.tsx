b5bef32472c2515cb4e2a620c9cdec682f24467b
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-11 18:48:49 +0000

    displayed data listings, completed listings page

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,12 +1,83 @@
-import React from "react";
-import { Text, View } from "react-native";
-
-const Listings = () => {
+import { defaultStyles } from "@/constants/Styles";
+import { Listing } from "@/types";
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  FlatList,
+  Image,
+  ListRenderItem,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+interface Props {
+  listings: any[];
+  category: string;
+}
+const Listings = ({ category, listings: items }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const listRef = useRef<FlatList>(null);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [category]);
+  const renderRow: ListRenderItem<Listing> = ({ item }) => (
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <View style={styles.listings}>
+          <Image source={{ uri: item.medium_url }} style={styles.image} />
+          <TouchableOpacity
+            style={{ position: "absolute", right: 30, top: 30 }}
+          >
+            <Ionicons name="heart-outline" size={24} color={"#000"} />
+          </TouchableOpacity>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
+              {item.name}
+            </Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: "mon-sb" }}>
+                {item.review_scores_rating / 20}
+              </Text>
+            </View>
+          </View>
+          <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+          <View style={{ flexDirection: "row", gap: 4 }}>
+            <Text style={{ fontFamily: "mon-sb" }}>€ {item.price}</Text>
+            <Text style={{ fontFamily: "mon" }}>night</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  );
   return (
-    <View>
-      <Text>Listings</Text>
+    <View style={defaultStyles.container}>
+      <FlatList
+        renderItem={renderRow}
+        ref={listRef}
+        data={loading ? [] : items}
+      />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  listings: {
+    padding: 16,
+    gap: 10,
+    marginVertical: 16,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
+});
 export default Listings;
