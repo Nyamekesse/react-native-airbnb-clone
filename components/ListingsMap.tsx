e8c75e6a691f4fd03a1c8144ed2bbc934d235abf
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2024-02-12 21:57:40 +0000

    created component to render ma view

---
+++ components/ListingsMap.tsx
@@ -0,0 +1,122 @@
+import { defaultStyles } from "@/constants/Styles";
+import { ListingGeo } from "@/types";
+import { useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-map-clustering";
+import { Marker } from "react-native-maps";
+
+interface Props {
+  listings: any;
+}
+
+const INITIAL_REGION = {
+  latitude: 37.33,
+  longitude: -122,
+  latitudeDelta: 9,
+  longitudeDelta: 9,
+};
+
+const ListingsMap = ({ listings }: Props) => {
+  const router = useRouter();
+  const onMarkerSelected = (item: ListingGeo) => {
+    router.push(`/listing/${item.properties.id}`);
+  };
+
+  const renderCluster = (cluster: any) => {
+    const { id, geometry, onPress, properties } = cluster;
+
+    const points = properties.point_count;
+    return (
+      <Marker
+        key={`cluster-${id}`}
+        coordinate={{
+          longitude: geometry.coordinates[0],
+          latitude: geometry.coordinates[1],
+        }}
+        onPress={onPress}
+      >
+        <View style={styles.marker}>
+          <Text
+            style={{
+              color: "#000",
+              textAlign: "center",
+              fontFamily: "mon-sb",
+            }}
+          >
+            {points}
+          </Text>
+        </View>
+      </Marker>
+    );
+  };
+  return (
+    <View style={defaultStyles.container}>
+      <MapView
+        animationEnabled={false}
+        clusterColor="#fff"
+        clusterTextColor="#000"
+        clusterFontFamily="mon-sb"
+        style={StyleSheet.absoluteFill}
+        provider={"google"}
+        showsUserLocation
+        showsMyLocationButton
+        renderCluster={renderCluster}
+        initialRegion={INITIAL_REGION}
+      >
+        {listings.features.map((item: ListingGeo) => (
+          <Marker
+            onPress={() => onMarkerSelected(item)}
+            key={item.properties.id}
+            coordinate={{
+              latitude: +item.properties.latitude,
+              longitude: +item.properties.longitude,
+            }}
+          >
+            <View style={styles.marker}>
+              <Text style={styles.markerText}>€ {item.properties.price}</Text>
+            </View>
+          </Marker>
+        ))}
+      </MapView>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  marker: {
+    padding: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    elevation: 5,
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+  markerText: {
+    fontSize: 14,
+    fontFamily: "mon-sb",
+  },
+  locateBtn: {
+    position: "absolute",
+    top: 70,
+    right: 20,
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+});
+export default ListingsMap;
